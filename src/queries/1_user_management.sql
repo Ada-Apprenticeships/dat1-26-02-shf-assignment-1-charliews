2e5ac6db38a4198923e3fdd7d7317ba43f843dbf
charliews <charlie.walker-saund@ada.ac.uk>
2026-02-28 16:37:23 +0000

    completed first and second set of queries

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -24,7 +24,20 @@ ORDER BY total_registrations DESC
 LIMIT 1;
 
 -- 1.5
-
+SELECT m.first_name, m.last_name, COUNT(*) AS total_registrations
+FROM class_attendance AS ca 
+JOIN members AS m ON ca.member_id = m.member_id
+GROUP BY ca.member_id
+ORDER BY total_registrations ASC
+LIMIT 1;
 
 -- 1.6
+SELECT COUNT(*)
+FROM(
+    SELECT member_id
+    FROM class_attendance
+    WHERE attendance_status = 'Attended'
+    GROUP BY member_id
+    HAVING COUNT(*) >= 2
+);
 
--- src/queries/2_payment_management.sql
+++ src/queries/2_payment_management.sql
@@ -1,11 +1,21 @@
 .open fittrackpro.db
-.mode column
+.mode box
 
 -- 2.1 
-
+INSERT INTO payments
+(member_id, amount, payment_date, payment_method, payment_type)
+VALUES
+(2, 60.00, '2025-02-10 10:00:00', 'Bank Transfer', 'Monthly membership fee');
 
 -- 2.2 
-
+SELECT strftime('%Y-%m', payment_date) AS month,
+    SUM(amount) AS total_revenue
+FROM payments
+WHERE payment_type = 'Monthly membership fee'
+GROUP BY month
+ORDER BY month;
 
 -- 2.3 
-
+SELECT *
+FROM payments
+WHERE payment_type = 'Day pass';
