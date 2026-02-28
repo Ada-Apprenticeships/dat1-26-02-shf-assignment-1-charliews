14135d09c0053faf0a9fe21805bb8e30414c0d41
charliews <charlie.walker-saund@ada.ac.uk>
2026-02-28 20:22:27 +0000

    done 2 queries on 3rd set

--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -1,11 +1,17 @@
 .open fittrackpro.db
-.mode column
+.mode box
 
 -- 3.1 
-
+SELECT equipment_id, name, type, next_maintenance_date
+FROM equipment 
+WHERE date(next_maintenance_date)
+BETWEEN date('now')
+AND date('now','+30 days');
 
 -- 3.2 
-
+SELECT type, COUNT(*) AS total_equipment
+FROM equipment 
+GROUP BY type;
 
 -- 3.3 
 
