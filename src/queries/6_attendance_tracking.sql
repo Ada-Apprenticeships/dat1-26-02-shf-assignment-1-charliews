2411f31e895fa4821abb754a5066e6b3747d130c
charliews <charlie.walker-saund@ada.ac.uk>
2026-03-02 19:44:12 +0000

    completed 6th set of queries - will need to go over 6.4

--- src/queries/6_attendance_tracking.sql
+++ src/queries/6_attendance_tracking.sql
@@ -1,14 +1,40 @@
 .open fittrackpro.db
-.mode column
+.mode box
 
 -- 6.1 
-
+INSERT INTO attendance (member_id, location_id, check_in_time)
+VALUES (7, 1, '2025-02-14 16:00:00');
 
 -- 6.2 
-
+SELECT 
+    DATE(check_in_time) AS visit_date,
+    check_in_time,
+    check_out_time
+FROM attendance
+WHERE member_id = 5;
 
 -- 6.3 
-
+SELECT 
+    strftime ('%w', check_in_time) AS day_of_week,
+    COUNT(*) AS visit_count
+FROM attendance
+GROUP BY strftime('%w', check_in_time)
+ORDER BY visit_count DESC
+LIMIT 1;
 
 -- 6.4 
-
+SELECT 
+    l.name AS location_name,
+    AVG(daily_count) AS avg_daily_attendance 
+FROM locations l
+LEFT JOIN (
+    SELECT 
+        location_id,
+        DATE(check_in_time) AS visit_date,
+        COUNT(*) AS daily_count
+    FROM attendance
+    GROUP BY location_id, DATE(check_in_time)
+)
+d 
+ON l.location_id = d.location_id
+GROUP BY l.location_id;
