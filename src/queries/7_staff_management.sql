f700f145fa73320e4c921759bcdd64a0075a156d
charliews <charlie.walker-saund@ada.ac.uk>
2026-03-03 16:58:54 +0000

    refactored last query of 6th set, completed 7th set

--- src/queries/7_staff_management.sql
+++ src/queries/7_staff_management.sql
@@ -1,8 +1,23 @@
 .open fittrackpro.db
-.mode column
+.mode box
 
 -- 7.1 
-
+SELECT 
+    staff_id,
+    first_name,
+    last_name,
+    position AS role
+FROM STAFF
+ORDER BY position;
 
 -- 7.2 
-
+SELECT 
+    s.staff_id AS trainer_id,
+    s.first_name || ' ' || s.last_name AS trainer_name,
+    COUNT(p.session_id) AS session_count
+FROM staff s
+JOIN personal_training_sessions p
+    ON s.staff_id = p.staff_id
+WHERE s.position = 'Trainer'
+AND p.session_date BETWEEN '2025-01-20' AND '2025-02-19'
+GROUP BY s.staff_id;
